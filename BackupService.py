e6be0ee94a135fc9cbcee5583ac3c5c21dfe635a
okingniko <okingniko@gmail.com>
2016-03-25 15:21:31 +0800

    Expand tabs to 4 blanks

--- BackupService.py
+++ BackupService.py
@@ -22,96 +22,96 @@ import json
 import logging
 
 class Backup():
-	def __init__(self, conf_file='./MainConf.json',log_file='./backup.log'):
-		self.conf_file = conf_file
-		self.log_file = log_file
+    def __init__(self, conf_file='./MainConf.json',log_file='./backup.log'):
+        self.conf_file = conf_file
+        self.log_file = log_file
 
-	def setlogger(self, log_file):
-		log_fmt = '%(levelname)s - %(asctime)s - %(message)s'
-		logging.basicConfig(
-			filename=log_file,
-			level=logging.DEBUG,
-			format=log_fmt)
+    def setlogger(self, log_file):
+        log_fmt = '%(levelname)s - %(asctime)s - %(message)s'
+        logging.basicConfig(
+            filename=log_file,
+            level=logging.DEBUG,
+            format=log_fmt)
 
-	def run(self):
-		self.setlogger(self.log_file)
-		with open(self.conf_file, 'r') as f:
-			data = json.load(f)
-		# Process each configure for backup_conf
-		for back_conf in data['backup_conf']:
-			local_dir = back_conf['local_dir'].encode('utf-8')
-			# local host can be any platform.
-			if os.path.isdir(local_dir) and local_dir[-1] != os.sep:
-				local_dir += os.sep
-			local_files = back_conf['local_files']
+    def run(self):
+        self.setlogger(self.log_file)
+        with open(self.conf_file, 'r') as f:
+            data = json.load(f)
+        # Process each configure for backup_conf
+        for back_conf in data['backup_conf']:
+            local_dir = back_conf['local_dir'].encode('utf-8')
+            # local host can be any platform.
+            if os.path.isdir(local_dir) and local_dir[-1] != os.sep:
+                local_dir += os.sep
+            local_files = back_conf['local_files']
 
-			server_ip = back_conf['server_ip'].encode('utf-8')
-			server_port = 22
-			remote_dir = back_conf['remote_dir'].encode('utf-8')
-			# remote server under linux environ.
-			if remote_dir[-1] != '/':
-				remote_dir += os.sep
+            server_ip = back_conf['server_ip'].encode('utf-8')
+            server_port = 22
+            remote_dir = back_conf['remote_dir'].encode('utf-8')
+            # remote server under linux environ.
+            if remote_dir[-1] != '/':
+                remote_dir += os.sep
 
-			user_name = back_conf['user_name']
-			user_passwd = back_conf['user_password']
-			# print type(local_dir), server_ip, server_port, type(remote_dir), user_name, user_passwd
+            user_name = back_conf['user_name']
+            user_passwd = back_conf['user_password']
+            # print type(local_dir), server_ip, server_port, type(remote_dir), user_name, user_passwd
 
-		try:
-			t = paramiko.Transport((server_ip, server_port))
-			t.connect(username=user_name, password=user_passwd)
-			sftp = paramiko.SFTPClient.from_transport(t)
+        try:
+            t = paramiko.Transport((server_ip, server_port))
+            t.connect(username=user_name, password=user_passwd)
+            sftp = paramiko.SFTPClient.from_transport(t)
 
-			# if remote_dir not exist, make it.
-			try:
-				sftp.normalize(remote_dir)
-			except Exception as e:
-				sftp.mkdir(remote_dir)
+            # if remote_dir not exist, make it.
+            try:
+                sftp.normalize(remote_dir)
+            except Exception as e:
+                sftp.mkdir(remote_dir)
 
-			# backup local files
-			for localf in local_files:
-				remote_file = os.path.join(remote_dir, os.path.split(localf)[1])
-				try:
-					sftp.put(localf, remote_file)
-				except Exception as e:
-					logging.error("Backup localfile %s: %s", localf, e)
-					print "ERROR: Backup localfile %s: %s" % (localf, e)
-				logging.info("Backup localfile %s to %s on %s ...", localf, remote_file, server_ip)
-				print "INFO: Backup localfile %s to %s on %s ..." % (localf, remote_file, server_ip)
+            # backup local files
+            for localf in local_files:
+                remote_file = os.path.join(remote_dir, os.path.split(localf)[1])
+                try:
+                    sftp.put(localf, remote_file)
+                except Exception as e:
+                    logging.error("Backup localfile %s: %s", localf, e)
+                    print "ERROR: Backup localfile %s: %s" % (localf, e)
+                logging.info("Backup localfile %s to %s on %s ...", localf, remote_file, server_ip)
+                print "INFO: Backup localfile %s to %s on %s ..." % (localf, remote_file, server_ip)
 
-			# backup files under local_dir(recursively).
-			for root, dirs, files in os.walk(local_dir):
-				for f in files:
-					local_file = os.path.join(root, f)
-					relative_path = local_file.replace(local_dir,'').replace('\\', '/')
-					remote_file = os.path.join(remote_dir, relative_path)
-					# print local_file, type(relative_path), remote_file
-					try:
-						sftp.put(local_file, remote_file)
-					except Exception as e:
-						remote_curr_dir = os.path.split(remote_file)[0]
-						sftp.mkdir(remote_curr_dir)
-						logging.info("Create directory %s on %s ...",remote_curr_dir, server_ip)
-						print "INFO: Create directory %s on %s ..." % (remote_curr_dir, server_ip)
-						sftp.put(local_file, remote_file)
+            # backup files under local_dir(recursively).
+            for root, dirs, files in os.walk(local_dir):
+                for f in files:
+                    local_file = os.path.join(root, f)
+                    relative_path = local_file.replace(local_dir,'').replace('\\', '/')
+                    remote_file = os.path.join(remote_dir, relative_path)
+                    # print local_file, type(relative_path), remote_file
+                    try:
+                        sftp.put(local_file, remote_file)
+                    except Exception as e:
+                        remote_curr_dir = os.path.split(remote_file)[0]
+                        sftp.mkdir(remote_curr_dir)
+                        logging.info("Create directory %s on %s ...",remote_curr_dir, server_ip)
+                        print "INFO: Create directory %s on %s ..." % (remote_curr_dir, server_ip)
+                        sftp.put(local_file, remote_file)
 
-					logging.info("Backup localfile %s to %s on %s ...",local_file, remote_file, server_ip)
-					print "INFO: Backup localfile %s to %s on %s ..." % (local_file, remote_file, server_ip)
-			t.close()
-		except Exception as e:
-			logging.error("Remote host %s %s",server_ip, e)
-			print "ERROR: Remote host %s %s" % (server_ip, e)
+                    logging.info("Backup localfile %s to %s on %s ...",local_file, remote_file, server_ip)
+                    print "INFO: Backup localfile %s to %s on %s ..." % (local_file, remote_file, server_ip)
+            t.close()
+        except Exception as e:
+            logging.error("Remote host %s %s",server_ip, e)
+            print "ERROR: Remote host %s %s" % (server_ip, e)
 
 class Download:
-	def __init__(self, conf_file='./MainConf.json'):
-		pass
+    def __init__(self, conf_file='./MainConf.json'):
+        pass
 
-	def run(self):
-		print "Download Method are under construction..."
-		pass
+    def run(self):
+        print "Download Method are under construction..."
+        pass
 
 def printintro():
-	print '''
-	         * ,MMM8&&&.            *
+    print '''
+             * ,MMM8&&&.            *
                   MMMM88&&&&&    .
                  MMMM88&&&&&&&
      *           MMM88&&&&&&&&
@@ -134,30 +134,30 @@ def printintro():
   |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
 
   Please Choose the Number of the method:
-	1. Backup
-	2. Download
-	3. :-)
+    1. Backup
+    2. Download
+    3. :-)
   '''
 
 def main():
-	printintro()
-	try:
-		num = int(input("The number: "))
-	except Exception as e:
-		print "ERROR: %s" % e
+    printintro()
+    try:
+        num = int(input("The number: "))
+    except Exception as e:
+        print "ERROR: %s" % e
 
-	if num == 1:
-		backup = Backup()
-		backup.run()
-	elif num == 2:
-		download = Download()
-		download.run()
-	elif num == 3:
-		print "If you have any idea, Please contact me at zrwang1993@126.com"
-	else:
-		print "Please follows the instructions..."
+    if num == 1:
+        backup = Backup()
+        backup.run()
+    elif num == 2:
+        download = Download()
+        download.run()
+    elif num == 3:
+        print "If you have any idea, Please contact me at zrwang1993@126.com"
+    else:
+        print "Please follows the instructions..."
 
-	print "More details can be seen at 'backup.log'..."
+    print "More details can be seen at 'backup.log'..."
 
 if __name__ == '__main__':
-	main()
+    main()
